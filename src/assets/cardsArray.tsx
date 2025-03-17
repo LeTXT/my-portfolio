3020b7270649848d5032ed9363db7bf9024bd391
Leandro <leandrogomesdeoliveira2@gmail.com>
2025-03-17 17:32:09 -0300

    add react calc

--- src/assets/cardsArray.tsx
+++ src/assets/cardsArray.tsx
@@ -3,17 +3,17 @@ export const cardsArray = [
     {
         title: 'Nimbus', 
         img: '/img/card/nimbus.png', 
-        language: ['JavaScript', 'React'], 
-        gitHub: ['https://github.com/LeTXT/Nimbus', 'https://www.google.com'], 
-        start: ['https://letxt.github.io/Nimbus/', 'https://www.bing.com']
+        language: ['React'], 
+        gitHub: ['https://github.com/LeTXT/Nimbus'], 
+        start: ['https://letxt.github.io/Nimbus/']
          
     }, 
     {
         title: 'Calculadora', 
         img: '/img/card/calculator.png', 
         language: ['JavaScript', 'React'], 
-        gitHub: ['https://github.com/LeTXT/calcNeoBrutalism'], 
-        start: ['https://letxt.github.io/calcNeoBrutalism/']
+        gitHub: ['https://github.com/LeTXT/calcNeoBrutalism', 'https://github.com/LeTXT/calculatorReactRhPages'], 
+        start: ['https://letxt.github.io/calcNeoBrutalism/', 'https://letxt.github.io/calculatorReactRhPages/']
          
     },
     {
@@ -24,13 +24,6 @@ export const cardsArray = [
         start: ['https://letxt.github.io/ticTacToeJS/']
          
     }, 
-    {
-        title: 'Jogo da Velha', 
-        img: '/img/card/ticTacToe.png', 
-        language: ['JavaScript'], 
-        gitHub: ['https://github.com/LeTXT/ticTacToeJS'], 
-        start: ['https://letxt.github.io/ticTacToeJS/']
-         
-    }, 
+     
     
 ]
