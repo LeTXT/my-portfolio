a381aaae5897ba5dbb08603f412e257dd21cffc5
Leandro <leandrogomesdeoliveira2@gmail.com>
2025-03-20 13:18:10 -0300

    modified path assets, and path package.json homepage

--- src/assets/cardsArray.tsx
+++ src/assets/cardsArray.tsx
@@ -2,7 +2,7 @@ export const cardsArray = [
     
     {
         title: 'Nimbus', 
-        img: '/img/card/nimbus.png', 
+        img: './img/card/nimbus.png', 
         language: ['React'], 
         gitHub: ['https://github.com/LeTXT/Nimbus'], 
         start: ['https://letxt.github.io/Nimbus/']
@@ -10,7 +10,7 @@ export const cardsArray = [
     }, 
     {
         title: 'Calculadora', 
-        img: '/img/card/calculator.png', 
+        img: './img/card/calculator.png', 
         language: ['JavaScript', 'React'], 
         gitHub: ['https://github.com/LeTXT/calcNeoBrutalism', 'https://github.com/LeTXT/calculatorReactRhPages'], 
         start: ['https://letxt.github.io/calcNeoBrutalism/', 'https://letxt.github.io/calculatorReactRhPages/']
@@ -18,7 +18,7 @@ export const cardsArray = [
     },
     {
         title: 'Jogo da Velha', 
-        img: '/img/card/ticTacToe.png', 
+        img: './img/card/ticTacToe.png', 
         language: ['JavaScript'], 
         gitHub: ['https://github.com/LeTXT/ticTacToeJS'], 
         start: ['https://letxt.github.io/ticTacToeJS/']
--- src/assets/languageArray.tsx
+++ src/assets/languageArray.tsx
@@ -1,7 +1,7 @@
 
 const language: string[] = ['JavaScript', 'HTML', 'CSS', 'TypeScript', 'React', 'Sass', 'Figma', 'Git', 'Python']
 
-const path: string = '/img/language/'
+const path: string = './img/language/'
 
 export const languageArray: { img: string; language: string }[] = []
 
